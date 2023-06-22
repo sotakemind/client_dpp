f8daa194d630e8535b10675cae871a833a7801a5
sotakemind <sotakemind@gmail.com>
2023-06-22 07:09:31 +0700

    f6
    f6

--- src/store/root-actions.ts
+++ src/store/root-actions.ts
@@ -1,10 +1,9 @@
-import { filtersSlice } from '../../app/explorer/filters/filters.slice'
+
 import { cartSlice } from './cart/cart.slice'
 import * as userActions from './user/user.actions'
 
 export const rootActions = {
     ...userActions,
     ...cartSlice.actions,
-    ...filtersSlice.actions
 
 }
